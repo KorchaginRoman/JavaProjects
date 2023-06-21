70e57e603095b5f6fad7fd799df4d7bbbd48cbba
KorchaginRoman <rkorchagin@ibs.ru>
2023-06-21 22:53:17 +0300

    Create Main.java

---
+++ TypesOfData/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double d = 4.9 * 10 * 10308;
+        byte b = 127;
+        System.out.println(d);
+        System.out.println(b);
+    }
+}
